930192bbf30993e99450a99a1d0e170fc89c7367
amit800IL <108745954+amit800IL@users.noreply.github.com>
2024-03-31 23:45:39 +0300

    few improvments

--- CPPFinalProject/CPPFinalProject.cpp
+++ CPPFinalProject/CPPFinalProject.cpp
@@ -14,6 +14,8 @@ using namespace std;
 
 void PickAndUseSystem(char input, shared_ptr<IServiceCustomerMediator> mailActionsManager);
 void UseSytem(char input, shared_ptr<IServiceCustomerMediator> mailActionsManager);
+void baseSTL(shared_ptr<IServiceCustomerMediator> mailActionsManager);
+void CustomSTL(shared_ptr<IServiceCustomerMediator> mailActionsManager);
 
 int main()
 {
@@ -59,62 +61,72 @@ void UseSytem(char input, shared_ptr<IServiceCustomerMediator> mailActionsManage
 {
 	if (input == '1')
 	{
-		unique_ptr<STLCustomerQueue> stlCustomerQueue = make_unique<STLCustomerQueue>();
-
-		unique_ptr<DateOfBirth> birthDate = make_unique<DateOfBirth>(11, 12, 1998);
-		unique_ptr<DateOfBirth> birthDate2 = make_unique<DateOfBirth>(17, 3, 1950);
-		unique_ptr<DateOfBirth> birthDate3 = make_unique<DateOfBirth>(3, 1, 2000);
-		unique_ptr<DateOfBirth> birthDate4 = make_unique<DateOfBirth>(17, 3, 1940);
-
-		unique_ptr<Customer> customer = make_unique<RegularCustomer>(*birthDate);
-		unique_ptr<Customer> customer2 = make_unique<ElderlyCustomer>(*birthDate2);
-		unique_ptr<Customer> customer3 = make_unique<RegularCustomer>(*birthDate3);
-		unique_ptr<Customer> customer4 = make_unique<ElderlyCustomer>(*birthDate4);
-
-		stlCustomerQueue->Enqueue(move(customer));
-		stlCustomerQueue->Enqueue(move(customer2));
-		stlCustomerQueue->Enqueue(move(customer3));
-		stlCustomerQueue->Enqueue(move(customer4));
-
-		while (!stlCustomerQueue->IsEmpty())
-		{
-			const unique_ptr<Customer>& topCustomer = stlCustomerQueue->customerPriorityQueue.front();
-
-			unique_ptr<Customer> customer = move(const_cast<unique_ptr<Customer>&>(topCustomer));
-
-			cout << "Called Customer number : " << customer->GetCustomerNumber() << endl;
-
-			mailActionsManager->CallCustomer(*customer);
-
-			stlCustomerQueue->customerPriorityQueue.pop();
-		}
+		baseSTL(mailActionsManager);
 	}
 
 	else if (input == '2')
 	{
-		shared_ptr<CustomerQueue> customerQueue = make_shared<CustomerQueue>();
+		CustomSTL(mailActionsManager);
+	}
+}
 
-		unique_ptr<DateOfBirth> birthDate = make_unique<DateOfBirth>(11, 12, 1998);
-		unique_ptr<DateOfBirth> birthDate2 = make_unique<DateOfBirth>(17, 3, 1950);
-		unique_ptr<DateOfBirth> birthDate3 = make_unique<DateOfBirth>(3, 1, 2000);
-		unique_ptr<DateOfBirth> birthDate4 = make_unique<DateOfBirth>(17, 3, 1940);
+void baseSTL(shared_ptr<IServiceCustomerMediator> mailActionsManager)
+{
+	unique_ptr<STLCustomerQueue> stlCustomerQueue = make_unique<STLCustomerQueue>();
 
-		unique_ptr<Customer> customer = make_unique<RegularCustomer>(*birthDate);
-		unique_ptr<Customer> customer2 = make_unique<ElderlyCustomer>(*birthDate2);
-		unique_ptr<Customer> customer3 = make_unique<RegularCustomer>(*birthDate3);
-		unique_ptr<Customer> customer4 = make_unique<ElderlyCustomer>(*birthDate4);
+	unique_ptr<DateOfBirth> birthDate = make_unique<DateOfBirth>(11, 12, 1998);
+	unique_ptr<DateOfBirth> birthDate2 = make_unique<DateOfBirth>(17, 3, 1950);
+	unique_ptr<DateOfBirth> birthDate3 = make_unique<DateOfBirth>(3, 1, 2000);
+	unique_ptr<DateOfBirth> birthDate4 = make_unique<DateOfBirth>(17, 3, 1940);
 
-		customerQueue->Enqueue(move(*customer));
-		customerQueue->Enqueue(move(*customer3));
-		customerQueue->Enqueue(move(*customer4));
-		customerQueue->Enqueue(move(*customer2));
+	unique_ptr<Customer> customer = make_unique<RegularCustomer>(*birthDate);
+	unique_ptr<Customer> customer2 = make_unique<ElderlyCustomer>(*birthDate2);
+	unique_ptr<Customer> customer3 = make_unique<RegularCustomer>(*birthDate3);
+	unique_ptr<Customer> customer4 = make_unique<ElderlyCustomer>(*birthDate4);
 
-		while (!customerQueue->IsEmpty())
-		{
-			CustomerQueueIterator customerIterator = customerQueue->begin();
-			Customer& customer = *customerIterator;
-			mailActionsManager->CallCustomer(customer);
-			customerQueue->Dequeue();
-		}
+	stlCustomerQueue->Enqueue(move(customer));
+	stlCustomerQueue->Enqueue(move(customer2));
+	stlCustomerQueue->Enqueue(move(customer3));
+	stlCustomerQueue->Enqueue(move(customer4));
+
+	while (!stlCustomerQueue->IsEmpty())
+	{
+		const unique_ptr<Customer>& topCustomer = stlCustomerQueue->customerPriorityQueue.front();
+
+		unique_ptr<Customer> customer = move(const_cast<unique_ptr<Customer>&>(topCustomer));
+
+		cout << "Called Customer number : " << customer->GetCustomerNumber() << endl;
+
+		mailActionsManager->CallCustomer(*customer);
+
+		stlCustomerQueue->customerPriorityQueue.pop();
+	}
+}
+
+void CustomSTL(shared_ptr<IServiceCustomerMediator> mailActionsManager)
+{
+	shared_ptr<CustomerQueue> customerQueue = make_shared<CustomerQueue>();
+
+	unique_ptr<DateOfBirth> birthDate = make_unique<DateOfBirth>(11, 12, 1998);
+	unique_ptr<DateOfBirth> birthDate2 = make_unique<DateOfBirth>(17, 3, 1950);
+	unique_ptr<DateOfBirth> birthDate3 = make_unique<DateOfBirth>(3, 1, 2000);
+	unique_ptr<DateOfBirth> birthDate4 = make_unique<DateOfBirth>(17, 3, 1940);
+
+	unique_ptr<Customer> customer = make_unique<RegularCustomer>(*birthDate);
+	unique_ptr<Customer> customer2 = make_unique<ElderlyCustomer>(*birthDate2);
+	unique_ptr<Customer> customer3 = make_unique<RegularCustomer>(*birthDate3);
+	unique_ptr<Customer> customer4 = make_unique<ElderlyCustomer>(*birthDate4);
+
+	customerQueue->Enqueue(move(*customer));
+	customerQueue->Enqueue(move(*customer3));
+	customerQueue->Enqueue(move(*customer4));
+	customerQueue->Enqueue(move(*customer2));
+
+	while (!customerQueue->IsEmpty())
+	{
+		CustomerQueueIterator customerIterator = customerQueue->begin();
+		Customer& customer = *customerIterator;
+		mailActionsManager->CallCustomer(customer);
+		customerQueue->Dequeue();
 	}
 }
